57dbcc07a59438c65d2dadf2a2a7d045848b6532
Fabio Weverton <fabiowsantos13@gmail.com>
2025-03-22 19:56:22 -0300

    media1

---
+++ media.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+    float a, b, MEDIA;
+    scanf("%f\n", &a);
+    scanf("%f\n", &b);
+    MEDIA = (a + b) / 2;
+    printf("%.5f\n", MEDIA);
+
+    return 0;
+}
