3a6bb3d0357a31c9d35a192d490dcd5dcd730f73
Fabio Weverton <fabiowsantos13@gmail.com>
2025-03-22 16:28:26 -0300

    Atividade bingo atualizada

--- bingo.c
+++ bingo.c
@@ -1,12 +1,6 @@
 #include <stdio.h>
 int main(){
-    int a = 3157;
-    printf("Digite o numero sorteado: ");
+    int a;
     scanf("%d", &a);
-
-
     printf("O numero sorteado foi: %d\n", a);
-
-    return 0;
-
 }
